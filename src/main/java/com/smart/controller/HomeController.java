6606eb4b563e1b07e3d6831ade82f786346eb7a3
shreySaxena <shreyyerhs07@gmail.com>
2024-01-12 19:41:47 +0530

    created base thymeleaf template

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -3,10 +3,26 @@ package com.smart.controller;
 import com.smart.entities.User;
 import com.smart.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("title","Question Manager");
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title","About");
+        return "about";
+    }
 
 }

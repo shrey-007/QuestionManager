2eb305b2132782fdf4d18e02f5832e394eea1af4
shreySaxena <shreyyerhs07@gmail.com>
2024-01-22 17:33:49 +0530

    diplaying questions using pagination

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -3,10 +3,14 @@ package com.smart.controller;
 import com.smart.entities.Question;
 import com.smart.entities.User;
 import com.smart.helper.Message;
+import com.smart.model.QuestionRepository;
 import com.smart.model.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,9 +22,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 @Controller
 public class UserController {
+    @Autowired
+    private QuestionRepository questionRepository;
     @Autowired
     private UserRepository userRepository;
     @RequestMapping("/addQuestion")
@@ -57,6 +64,7 @@ public class UserController {
                 Path path=Paths.get(savedFile.getAbsolutePath()+File.separator+image.getOriginalFilename());
                 Files.copy(image.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
             }
+            question.setUser(user);
 
             //add question to user's list
             user.getQuestions().add(question);
@@ -76,10 +84,20 @@ public class UserController {
     }
 
 
-    @GetMapping("/showQuestions")
-    public String showQuestions(Model model,HttpSession session){
+    //{page} is a dynamic variable which denotes ki pagination mai currently konse page pr ho
+    @GetMapping("/showQuestions/{page}")
+    public String showQuestions(@PathVariable("page") Integer page, Model model,HttpSession session){
         User user=(User) session.getAttribute("currentUser");
         model.addAttribute("user",user);
+
+        //this pageable contains information of current page and no. of questions per page
+        Pageable pageable =PageRequest.of(page,3);
+        Page<Question> questionList=questionRepository.findQuestionsByUser(user.getId(),pageable);
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(questionList);
+        model.addAttribute("questions",questionList);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("totalPages",questionList.getTotalPages());
         return "showQuestions";
     }
 

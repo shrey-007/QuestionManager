b4ef03bdfcd0bdddf7c4d5a74bbfce8c6f554bc0
shreySaxena <shreyyerhs07@gmail.com>
2024-08-19 18:24:10 +0530

    updated entities

--- src/main/java/com/smart/entities/Question.java
+++ src/main/java/com/smart/entities/Question.java
@@ -2,9 +2,15 @@ package com.smart.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "question")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,75 +25,6 @@ public class Question {
     @JsonIgnore
     //to avoid circular dependency we have used json ignore.
     private User user;
-
-    public int getQid() {
-        return qid;
-    }
-
-    public void setQid(int qid) {
-        this.qid = qid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getImageName() {
-        return imageName;
-    }
-
-    public void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Question() {
-    }
-
-    public Question(String name, String description, String fileName, String imageName, User user) {
-        this.name = name;
-        this.description = description;
-        this.fileName = fileName;
-        this.imageName = imageName;
-        this.user = user;
-    }
-
-    public Question(int qid, String name, String description, String fileName, String imageName, User user) {
-        this.qid = qid;
-        this.name = name;
-        this.description = description;
-        this.fileName = fileName;
-        this.imageName = imageName;
-        this.user = user;
-    }
 
     @Override
     public String toString() {
--- src/main/java/com/smart/entities/User.java
+++ src/main/java/com/smart/entities/User.java
@@ -2,12 +2,18 @@ package com.smart.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,84 +22,23 @@ public class User {
     @Size(min=3,max=25,message = "Minimum 3 and Maximum 25 characters allowed")
     private String name;
     private  String password;
-    @Column(unique = true)
+    @Column(unique = true,nullable = false)
     private String email;
-    private String imageUrl;
-    @Size(min=3,max=100,message = "Minimum 3 and Maximum 100 characters allowed")
-    private String about;
 
     private String toDo;
 
 
-    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
     private List<Question> questions=new ArrayList<>();
 
-    public User() {
-    }
+    private boolean isEnabled=false;
+    private boolean emailVerified=false;
+    private boolean phoneVerified=false;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
-
-    public List<Question> getQuestions() {
-        return questions;
-    }
-
-    public void setQuestions(List<Question> questions) {
-        this.questions = questions;
-    }
-
-    public String getToDo() {
-        return toDo;
-    }
-
-    public void setToDo(String toDo) {
-        this.toDo = toDo;
-    }
+    // from where did the user did signup
+    // By default sefl hai, means khud se login kra hai
+    private Providers provider=Providers.SELF;
+    private String providerUserId;
 
     @Override
     public String toString() {
@@ -102,8 +47,6 @@ public class User {
                 ", name='" + name + '\'' +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", about='" + about + '\'' +
                 ", questions=" + questions +
                 '}';
     }

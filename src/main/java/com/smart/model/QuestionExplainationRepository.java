5e6f7c8b9124561f4a142f1a0faf8150df775ea7
shreySaxena <shreyyerhs07@gmail.com>
2024-08-23 16:07:45 +0530

    added files to deploy

--- src/main/java/com/smart/model/QuestionExplainationRepository.java
+++ src/main/java/com/smart/model/QuestionExplainationRepository.java
@@ -2,7 +2,9 @@ package com.smart.model;
 
 import com.smart.entities.QuestionExplaination;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionExplainationRepository extends MongoRepository<QuestionExplaination,Integer> {
     public QuestionExplaination findByQid(int qid);
 }
